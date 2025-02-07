1b2273810fc90921a2f9ced01daef78d00856a7a
Tommaso Lo Piparo <t.lopiparo@interjob.it>
2025-02-07 15:50:29 +0100

    Add API routes for orders and products; remove unused user migration

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+
+Route::apiResource('orders', OrderController::class);
+
+Route::apiResource('products', ProductController::class);
 
 Route::get('/ping', function () {
     return response()->json(['message' => 'API is working!']);
